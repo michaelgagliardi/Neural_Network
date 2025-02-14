5d4e2c9d390254f79d6dade461b470fbae9e6d6b
Michael Gagliardi <michael.gagliardi@ibm.com>
2025-02-13 23:46:32 -0500

    inital commit - adding initial classes, functions and data set for forward prop

---
+++ layers.py
@@ -0,0 +1,64 @@
+import numpy as np
+import math
+import sys
+import matplotlib
+from cd import create_dataset
+
+class layer_dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class act_relu:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class act_softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilites = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilites
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+    
+class loss_categorical_cross_ent(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
+X, y = create_dataset(points=100, classes=3)
+
+dense1 = layer_dense(2,3)
+activation1 = act_relu()
+
+dense2 = layer_dense(3,3)
+activation2 = act_softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+loss_function = loss_categorical_cross_ent()
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss:", loss)
